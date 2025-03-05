e1b22ccb9ad7789481584e560d3efd7483b842a2
lgpaula <l.gdepaula@hotmail.com>
2025-03-05 11:50:41 +0100

    cleaned paths

--- scraper/utils.py
+++ scraper/utils.py
@@ -20,9 +20,21 @@ class Title:
     schedule: str = None
     companies: str = None
 
+# dicts or nosql?
 dict_id_stars = {}
 dict_id_genre = {}
 dict_id_writer = {}
 dict_id_company = {}
 dict_id_creator = {}
 dict_id_director = {}
+
+# x-path & class strings
+@dataclass(frozen = True)
+class XPaths:
+    banner_element = ".icb-btn.sc-bcXHqe.sc-hLBbgP.sc-ftTHYK.dcvrLS.dufgkr.ecppKW"
+    see_more_button = "ipc-see-more__text"
+    scroll_into_view = "arguments[0].scrollIntoView(true);"
+    multi_title_parent = "sc-e22973a9-0"
+    multi_title_item = "ipc-metadata-list-summary-item"
+    single_title_parent_1 = "sc-9a2a0028-0"
+    single_title_parent_2 = '//script[@id="__NEXT_DATA__"]'
