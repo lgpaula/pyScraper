d85b8e6179aa59d75c4ede11967d77f65b20f1c6
lgpaula <l.gdepaula@hotmail.com>
2025-05-04 14:50:09 +0200

    Fixed waiting time

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -44,10 +44,14 @@ def scrape_multiple_titles(url: str):
 
         # Click "See more" button (maybe multiple times)
         try:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+
             button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, XPaths.see_more_button)))
             driver.execute_script(XPaths.scroll_into_view, button)
             button.click()
             # Wait until new items are loaded
+            time.sleep(1)
             wait.until(lambda d: len(d.find_elements(By.CLASS_NAME, XPaths.multi_title_item)) > 0)
         except Exception as e:
             print("No button found or click failed:", e)
@@ -157,6 +161,7 @@ def custom_search_url(params: dict) -> str:
 if __name__ == "__main__":
     # scrape_single_title("tt2085059")
 
+    start_time = time.time()
     criteria = "https://www.imdb.com/search/title/?title_type=feature,tv_series"
 
     if (len(sys.argv) > 1):
@@ -167,6 +172,9 @@ if __name__ == "__main__":
     create_table()
     for movie in movies:
         insert_title(movie)
+
+    final_time = time.time() - start_time
+    print (final_time)
 
     # print(fetch_episode_dates("tt31510819", 1))
 
