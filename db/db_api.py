82c3426975bb42a08ca904712c1a7382065df119
lgpaula <l.gdepaula@hotmail.com>
2025-07-29 21:53:22 -0300

    Health check

--- db/db_api.py
+++ db/db_api.py
@@ -1,3 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Blueprint, request, jsonify
 import json
 import logging
+
+db_bp = Blueprint('database', __name__)
+
+@db_bp.route("/health", methods=["GET"])
+def health():
+    logging.info("Health check requested.")
+    return jsonify({"status": "running"}), 200
